4e8f3172875bbd56c6680e6c147b75f2308d77ad
jgamboa17 <jgamboa17@toromail.csudh.edu>
2024-09-26 17:13:43 -0700

    UPDATED LAB #4

--- jgamboa4.c
+++ jgamboa4.c
@@ -1,7 +1,7 @@
 /*
  * Jonathan Gamboa
  * CSC 341
- * 09/18/2024
+ * 09/26/2024
  * jgamboa4.c
  */
 
@@ -13,23 +13,16 @@ int y = 15;
 
 int main(int argc, char *argv[]){
 	int i;
-	int z =0;
-	
-	int array1[5] = {0};
-	
-	int *pointer1;
-	pointer1 = &z;
+	int *value;
+	value = (int *)malloc(sizeof(int)*5);
+	for(i = 0; i < 5; i++)
+		value[i] = i;
 	
 	printf("The memory address of argc is: %p \n", &argc);
- 	
-
-	printf("The value of variable z is %d \n", z);
-	printf("The address of variable z is %p \n", &z);
-	printf("The value of pointer1 = %p which is the same as the address of z which is %p \n", pointer1, &z);
-	
-	for(i = 0; i<5; i++){
-		printf("Array1[%d] = %d with address of %p \n", i, array1[i], &array1[i]);
-	}
+	printf("The memeory address of the stack variable j is : %p \n", &i);
+	printf("The memory address of the heap  variable value is %p \n", &value);
+	printf("The memory address of the uninitialize data of x is: %p \n",&x);
+        printf("The memory address of the initialized data of y: %p \n", &y);
 
 	return EXIT_SUCCESS;
 }
